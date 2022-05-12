01b7d7076aeab7e926214fb3acee899a24a6161e
prathik manchikalapudi <prathikmanchikalapudi7@gmail.com>
2022-05-12 16:00:39 +0530

    added api link

---
+++ src/Chat.js
@@ -0,0 +1,86 @@
+import { Avatar,IconButton } from '@mui/material';
+import React from 'react';
+import "./Chat.css";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';   
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import AttachFileIcon from '@mui/icons-material/AttachFile' 
+import MicIcon from '@mui/icons-material/Mic'
+import {useState} from "react";
+import axios from "./axios";
+
+function Chat( {messages}) {
+
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    axios.post('/messages/new', {
+      "message" : input,
+      "name" : " prathik ",
+      "timestamp" : "12th may",
+      "received" : false,
+    });
+
+    setInput("");
+
+  }; 
+
+  return (
+    <div className='chat'>
+       <div className='chat__header'>
+        <Avatar />
+        <div className='chat__headerInfo'>
+            <h3>Room Name</h3>
+            <p>Last Seen at ...</p>
+        </div>
+
+        <div className='chat__headerRight'>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <IconButton>
+           <MoreVertIcon /> 
+          </IconButton>
+        </div>
+
+       </div>
+       <div className='chat__body'>
+         {messages.map((message) => (
+           <p className={`chat__message ${message.received && 'chat__receiver'}`}>
+               
+            <span className='chat__name'>{message.name}</span>   
+               {message.message}
+            <span className='chat__timestamp'>
+              {message.timestamp}  
+            </span>
+ 
+            
+            </p>
+
+
+         ))}
+          
+       </div>
+       <div className='chat__footer'>
+           <InsertEmoticonIcon />
+           <AttachFileIcon />
+            <form>
+                <input 
+                value={input} 
+                onChange = {(e) => setInput(e.target.value)}   
+                placeholder='Type your Message'
+                type='text' 
+                 />
+                <button onClick={sendMessage} type="submit"> 
+                    Send a Message
+                </button>
+            </form>
+            <MicIcon />
+       </div>
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-HUuh6D4nqIr1rG5L8GtEt8UfxM1v4KE",
+  authDomain: "chatter-mern-10b7c.firebaseapp.com",
+  projectId: "chatter-mern-10b7c",
+  storageBucket: "chatter-mern-10b7c.appspot.com",
+  messagingSenderId: "579805352359",
+  appId: "1:579805352359:web:7438f237160704bb27a682"
+};
